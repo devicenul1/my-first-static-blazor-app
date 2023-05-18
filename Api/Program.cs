0d23bacf5a9c17c3dffaae3bf6a842531edbe640
William Burress <william.burress@rsmus.com>
2023-05-18 15:46:47 -0400

    more auth

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using System;
 
 namespace ApiIsolated
 {
@@ -10,10 +13,21 @@ namespace ApiIsolated
         public static void Main()
         {
             var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureFunctionsWorkerDefaults(builder =>
+                {
+                    // builder
+                    //     .Services
+                    //         .addop
+                    //         .AddMicrosoftIdentityWebApiAuthentication(Environment.get System.Configuration.GetSection("AzureAd"));
+                })
+                .ConfigureServices(s =>
+                {
+                })
                 .Build();
 
             host.Run();
+
+            host.Services.add
         }
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("read-write");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
